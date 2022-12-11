ffa919f2d3d141095b67a3a4fe214484392c52a6
khalid <khalid.ibr212@gmail.com>
2022-12-11 20:27:59 +0300

    Avarage Calculation Fix

--- src/Components/ReportTable/ReportTable.jsx
+++ src/Components/ReportTable/ReportTable.jsx
@@ -5,45 +5,32 @@ import { useSelector } from "react-redux";
 import { getRewatiDataByDate } from "../../supabase";
 import "./ReportTable.css";
 
-function ReportTable({ title, property }) {
-  const [TotaLSubmissions, SetTotaLSubmissions] = useState(0);
-  const totalSubmissioncounter = async () => {
-    SetTotaLSubmissions(
-      JSON.parse(
-        JSON.stringify(useSelector((state) => state.adminstat).totalSubmissions)
-      )
-    );
-  };
-  useEffect(() => {
-    totalSubmissioncounter();
-  }, [useSelector((state) => state.adminstat).totalSubmissions]);
-  const totalNumberOfSubmissions = TotaLSubmissions ? TotaLSubmissions : 100;
-
-  const [percentageColor, setpercentageColor] = React.useState("green");
+function ReportTable({ title, property, p }) {
+  const totalNumberOfSubmissions = useSelector(
+    (state) => state.adminstat
+  ).totalSubmissions;
+  console.log(totalNumberOfSubmissions);
   const total =
     property.filter((el) => el === "0 days").length +
     property.filter((el) => el === "1-2 days").length +
     property.filter((el) => el === "3-4 days").length +
     property.filter((el) => el === "5-6 days").length +
     property.filter((el) => el === "7 days").length;
-  // function percentage(percent, total) {
-  //     return ((percent/ 100) * total)
-  // }
   function percentageCalculator(numA, numB) {
-    return (numA / numB) * TotaLSubmissions;
+    return (numA / numB) * 100;
   }
 
   const totalPercentage = percentageCalculator(total, property.length);
   const sevenDays = property.filter((el) => el === "7 days").length * 100;
   const sixDays = property.filter((el) => el === "5-6 days").length * 85;
   const fourDays = property.filter((el) => el === "3-4 days").length * 60;
-  const twoDays = property.filter((el) => el === "1-2 days").length * 30;
+  const twoDays = property.filter((el) => el === "1-2 days").length * 0;
   const zeroDays = property.filter((el) => el === "0 days").length * 0;
-  // const totalNumberOfSubmissions = localStorage.getItem("totalSubmission");
-  console.log(totalNumberOfSubmissions);
+  // console.log(totalNumberOfSubmissions);
   const totalScore =
     (sevenDays + sixDays + fourDays + twoDays + zeroDays) /
-    100;
+    totalNumberOfSubmissions;
+  // console.log("ht"+p)
 
   return (
     <div className="reportTable">
@@ -67,6 +54,7 @@ function ReportTable({ title, property }) {
           {property.filter((el) => el === "1-2 days").length}
         </div>
         <div className="percentage">
+          {p}
           {percentageCalculator(
             property.filter((el) => el === "1-2 days").length,
             total
